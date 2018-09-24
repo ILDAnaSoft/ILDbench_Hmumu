6e32f099638510e6141dace331b6bd815a434667
shkawada <skawada@naf-ilc13.desy.de>
2018-09-24 17:44:56 +0200

    just added for other DBD-style analysis codes

---
+++ DBD/qqh250/mymarlin/src/Sphericity.cc
@@ -0,0 +1,203 @@
+#include "Sphericity.h"
+#include <iostream>
+#include <vector>
+#include "jama_eig.h"
+#include "tnt_math_utils.h"
+#ifdef MARLIN_USE_AIDA
+#include <marlin/AIDAProcessor.h>
+#include <AIDA/IHistogramFactory.h>
+#include <AIDA/ICloud1D.h>
+//#include <AIDA/IHistogram1D.h>
+#endif
+#include "IMPL/LCEventImpl.h" 
+#include "IMPL/LCCollectionVec.h"
+#include "IMPL/SimCalorimeterHitImpl.h"
+#include "IMPL/CalorimeterHitImpl.h"
+#include "IMPL/MCParticleImpl.h" 
+#include "IMPL/TrackerHitImpl.h" 
+#include "IMPL/TrackImpl.h" 
+#include "IMPL/ClusterImpl.h" 
+#include "IMPL/ReconstructedParticleImpl.h" 
+#include "IMPL/ParticleIDImpl.h" 
+#include "IMPL/LCFlagImpl.h" 
+
+#include "IMPL/LCRelationImpl.h"
+
+
+#include <EVENT/LCCollection.h>
+#include <EVENT/ReconstructedParticle.h>
+#include <marlin/Exceptions.h>
+
+using namespace lcio ;
+using namespace marlin ;
+using namespace std;
+using namespace JAMMA;
+using namespace TNT;
+Sphericity  aSphericity ;
+
+
+Sphericity::Sphericity() : Processor("Sphericity") {
+  
+  // modify processor description
+  _description = "Sphericity calculates eigenvalues of sphericity tensor" ;
+  
+   
+  // register steering parameters: name, description, class-variable, default value
+
+  registerInputCollection( LCIO::RECONSTRUCTEDPARTICLE, 
+			   "CollectionName" , 
+			   "Name of the ReconstructedParticle collection"  ,
+			   _colName ,
+			   std::string("RecontructedParticle") ) ;
+  
+  registerProcessorParameter("r_value",
+                             " exponent in sphericity tensor use 2.0 for classical 1.0 for C,D",
+			     _r,
+                             float(2.0));
+  registerProcessorParameter("eigenvalues_parameter_name",
+                             "name of parameter to store the results ",
+                             _dumpobjectname,
+			     std::string("sphericity_tensor_eigenvalues"));
+
+}
+
+
+void Sphericity::init() { 
+
+  // usually a good idea to
+  printParameters() ;
+  
+  _nRun = 0 ;
+  _nEvt = 0 ;
+  
+}
+
+void Sphericity::processRunHeader( LCRunHeader* run) { 
+
+  _nRun++ ;
+} 
+
+void Sphericity::processEvent( LCEvent * evt ) { 
+  //cout << "This is event/run " << evt->getEventNumber() <<" / "<< evt->getRunNumber() <<std::endl;
+  //cout << "The process in this event is: " << evt->parameters().getStringVal( "Process") << endl ; 
+
+   // this gets called for every event 
+  // usually the working horse ...
+ 
+//   float Pduzi; 
+ double Pduzi; 
+  float sp[3][3];
+  float norm=0.0;
+//   float *pp;
+  const double *pp ;
+//   float dvojka=2.0;
+  double dvojka=2.0;
+
+  ReconstructedParticle* p;
+  
+  
+  // fill histogram from LCIO data :
+
+  LCCollection* col = 0;
+  
+  try{ col = evt -> getCollection( _colName ) ; }
+  catch(EVENT::DataNotAvailableException){
+    cout << "Cannot find PFO Collection in event/run  " << evt->getEventNumber() <<" / "<< evt->getRunNumber() <<std::endl;
+    cout << "Skipping this event!" << endl;
+    throw marlin::SkipEventException(this);
+  }
+
+  if (col->getNumberOfElements() == 0){
+    cout << "PFO Collection is empty in event/run  " << evt->getEventNumber() <<" / "<< evt->getRunNumber() <<std::endl;
+    return;
+  }
+  
+     for(int i=0;i<3;i++)
+       {
+    for (int j=0;j<3;j++)       
+       {     
+         sp[j][i]=0.0;
+       }
+       }
+
+  if( col != 0 ){
+    
+    int nRecP = col->getNumberOfElements()  ;
+    
+    for(int kk=0; kk< nRecP ; kk++){
+      p=dynamic_cast<ReconstructedParticle*>( col->getElementAt( kk ) );
+
+//       pp=const_cast<float*>(p->getMomentum());
+      pp = p->getMomentum() ;
+
+     Pduzi=sqrt(pow(pp[0],dvojka)+pow(pp[1],dvojka)+pow(pp[2],dvojka));
+
+     norm=norm+pow(Pduzi,(double) _r); 
+        for(int j=0;j<3;j++)      
+	   {
+	for(int i=0;i<3;i++)     
+	   {
+	  sp[j][i]=sp[j][i]+pp[i]*pp[j]*pow(Pduzi,(_r-dvojka));
+           }	
+	   }
+         
+    } 
+  }
+
+      for(int j=0;j<3;j++)      
+	   {
+	for(int i=0;i<3;i++)     
+	   {
+	     sp[j][i]=sp[j][i]/norm;
+	     //  cout << "sp tenzor "<< sp[j][i] << endl;
+           }	
+	   }
+        float lre[3];
+	Eigenvalue test(sp);
+	test.getRealEigenvalues(lre);
+	// cout << lre[0] << "   " << lre[1] << "  " << lre[2] << endl;  
+        float sphericity;
+        float aplanarity;
+        string out_name;
+         sphericity=1.5*(lre[0]+lre[1]);
+         aplanarity=1.5*lre[0];
+         FloatVec udri_me_do_zore;
+         udri_me_do_zore.push_back(lre[0]);
+	 udri_me_do_zore.push_back(lre[1]);
+	 udri_me_do_zore.push_back(lre[2]);
+	 if (_r==2.0){
+         col->parameters().setValue("sphericity",sphericity);
+	 col->parameters().setValue("aplanarity",aplanarity);
+	 col->parameters().setValues(_dumpobjectname.c_str(),udri_me_do_zore);
+         }
+	 if (_r==1.0){
+             float ccc;
+             float ddd;
+	     ccc=3.0*(lre[0]*lre[1]+lre[0]*lre[2]+lre[2]*lre[1]);
+	      ddd=27.0*lre[0]*lre[1]*lre[2];
+         col->parameters().setValue("C",ccc);
+	 col->parameters().setValue("D",ddd);
+	 col->parameters().setValues(_dumpobjectname.c_str(),udri_me_do_zore);
+         }
+         else 
+         {
+	     col->parameters().setValues(_dumpobjectname.c_str(),udri_me_do_zore);
+         }
+  _nEvt ++ ;
+}
+
+
+
+void Sphericity::check( LCEvent * evt ) { 
+  // nothing to check here - could be used to fill checkplots in reconstruction processor
+}
+
+
+void Sphericity::end(){ 
+  
+//   std::cout << "MyProcessor::end()  " << name() 
+// 	    << " processed " << _nEvt << " events in " << _nRun << " runs "
+// 	    << std::endl ;
+
+}
+
