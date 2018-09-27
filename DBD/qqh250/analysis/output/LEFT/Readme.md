e7d7a59d74cf4d8ff374cbe4cdfefd9d0cbe578f
shkawada <skawada@naf-ilc12.desy.de>
2018-09-27 17:14:50 +0200

    added core files

--- DBD/qqh250/analysis/output/LEFT/Readme.md
+++ DBD/qqh250/analysis/output/LEFT/Readme.md
@@ -1,3 +1,9 @@
+# How To Use
+- preparation
+  -- condor_submit doing.submit (DESY-specific)
+  --Or do `. doing.sh`
+
 # Explanation
 This place is used for further preparation and cut-based analysis.
 
+## Each file
---
+++ DBD/qqh250/analysis/output/LEFT/doing.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+source /afs/desy.de/user/s/skawada/sonas_work/stage_qqh250/analysis/init_ilcsoft.sh
+cd /afs/desy.de/user/s/skawada/sonas_work/stage_qqh250/analysis/output/LEFT
+
+root -l -b alldata_select.root <<EOF
+.L AddWeight.C+
+MakePolInfo("procid250.txt")
+AddWeight("alldata_select.root",hev)
+.q
+EOF
+
+sleep 5
+
+root -l -b <<EOF
+.L SkimCut.C+
+SkimCut("alldata_select.root","alldata_wo.root","dataTree","!((processid>=106475&&processid<=106486)&&abs(higgsdecay1)==13)")
+.q
+EOF
+
+sleep 5
+
+root -l -b <<EOF
+.L SkimCut.C+
+SkimCut("alldata_wo.root","alldata_cut1.root","dataTree","n_muminus==1&&n_muplus==1")
+.q
+EOF
