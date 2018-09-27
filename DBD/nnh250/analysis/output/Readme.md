2d81aac0f250d0e3a1b9ee67412acf0581f83a30
shkawada <skawada@naf-ilc12.desy.de>
2018-09-27 16:22:27 +0200

    added core files and readme

---
+++ DBD/nnh250/analysis/output/Readme.md
@@ -0,0 +1,35 @@
+# Caution
+This part contains many DESY-specific part.
+You need to be careful.
+
+# How To Use
+- Create `LEFT/` and `RIGHT/`
+- condor_submit doing.submit (DESY-specific)
+- Or, do `. doing.sh` (Not recommend to do this in worker node at usual working time because it takes very long time. Do it in midnight or create your own job submission script and submit it is much better for your colleague.)
+
+## Step by step case
+```
+. hadd.sh
+root -l -b
+.L SkimVar.C+
+SkimVar("alldata1.root","alldata1_select.root","dataTree","var1:var2:var3:...")
+SkimVar("alldata2.root","alldata2_select.root","dataTree","var1:var2:var3:...")
+.q
+. hadd2.sh
+root -l -b
+.L proc.C+
+process()
+.q
+cp alldata_select.root LEFT/.
+cp alldata_select.root RIGHT/.
+```
+
+# Explanation
+This place is used for prepation of analysis: combine all small root files to single large root file, extract necessary variables, add process ID histogram, and copy the processed root file at `LEFT/.` and `RIGHT/.`.
+This is my favorite style, you can do it more clever way I think.
+
+## Each file
+- `hadd.sh`,`hadd2.sh`: for combining each root file into single large file (When the total file size is too huge, then ROOT will split this huge file. This is the reason why I have 2 scripts for merging.)
+- `SkimVar.C`: for extracting necessary variables from root file, somehow self-documented
+- `proc.C`: creating one histogram of process ID, will be used for further prepartion
+- `selectvar.txt`: it would be useful to summarize your necessary variables, and easy to use `SkimVar.C`, just copy-and-paste it
---
+++ DBD/nnh500/analysis/output/hadd.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+echo "START hadd..."
+
+echo "combining signal process"
+hadd hmumu.root hmumu/hmumu-???.root
+
+echo "combining ffh background"
+hadd higgs_ffh.root higgs_ffh/higgs_ffh-???.root
+
+echo "combining 2f backgrounds"
+hadd 2f_Z_bhabhag.root 2f_Z_bhabhag/2f_Z_bhabhag-???.root
+hadd 2f_Z_hadronic.root 2f_Z_hadronic/2f_Z_hadronic-???.root
+hadd 2f_Z_leptonic.root 2f_Z_leptonic/2f_Z_leptonic-???.root
+echo "combine into single 2f file"
+hadd 2f_all.root 2f_Z_bhabhag.root 2f_Z_hadronic.root 2f_Z_leptonic.root
+echo "deleting intermediate files"
+rm 2f_Z_bhabhag.root 2f_Z_hadronic.root 2f_Z_leptonic.root
+
+echo "combining 4f backgrounds"
+hadd 4f_singleW_leptonic.root 4f_singleW_leptonic/4f_singleW_leptonic-???.root
+hadd 4f_singleW_semileptonic.root 4f_singleW_semileptonic/4f_singleW_semileptonic-???.root
+hadd 4f_singleZee_leptonic.root 4f_singleZee_leptonic/4f_singleZee_leptonic-???.root
+hadd 4f_singleZee_semileptonic.root 4f_singleZee_semileptonic/4f_singleZee_semileptonic-???.root
+hadd 4f_singleZnunu_leptonic.root 4f_singleZnunu_leptonic/4f_singleZnunu_leptonic-???.root
+hadd 4f_singleZnunu_semileptonic.root 4f_singleZnunu_semileptonic/4f_singleZnunu_semileptonic-???.root
+hadd 4f_singleZsingleWMix_leptonic.root 4f_singleZsingleWMix_leptonic/4f_singleZsingleWMix_leptonic-???.root
+hadd 4f_WW_leptonic.root 4f_WW_leptonic/4f_WW_leptonic-???.root
+hadd 4f_WW_semileptonic.root 4f_WW_semileptonic/4f_WW_semileptonic-???.root
+hadd 4f_WW_hadronic.root 4f_WW_hadronic/4f_WW_hadronic-???.root
+hadd 4f_ZZ_leptonic.root 4f_ZZ_leptonic/4f_ZZ_leptonic-???.root
+hadd 4f_ZZ_semileptonic.root 4f_ZZ_semileptonic/4f_ZZ_semileptonic-???.root
+hadd 4f_ZZ_hadronic.root 4f_ZZ_hadronic/4f_ZZ_hadronic-???.root
+hadd 4f_ZZWWMix_leptonic.root 4f_ZZWWMix_leptonic/4f_ZZWWMix_leptonic-???.root
+echo "combine into single 4f file and delete intermediate files"
+hadd 4f_all.root 4f_singleW_leptonic.root 4f_singleW_semileptonic.root 4f_singleZee_leptonic.root 4f_singleZee_semileptonic.root 4f_singleZnunu_leptonic.root 4f_singleZnunu_semileptonic.root 4f_singleZsingleWMix_leptonic.root 4f_WW_leptonic.root 4f_WW_semileptonic.root 4f_WW_hadronic.root 4f_ZZ_leptonic.root 4f_ZZ_semileptonic.root 4f_ZZ_hadronic.root 4f_ZZWWMix_leptonic.root
+echo "delete intermediate files"
+rm 4f_singleW_leptonic.root 4f_singleW_semileptonic.root 4f_singleZee_leptonic.root 4f_singleZee_semileptonic.root 4f_singleZnunu_leptonic.root 4f_singleZnunu_semileptonic.root 4f_singleZsingleWMix_leptonic.root 4f_WW_leptonic.root 4f_WW_semileptonic.root 4f_WW_hadronic.root 4f_ZZ_leptonic.root 4f_ZZ_semileptonic.root 4f_ZZ_hadronic.root 4f_ZZWWMix_leptonic.root
+
+echo "combining 5f background"
+hadd 5f.root 5f/5f-???.root
+
+echo "combine aa_4f background"
+hadd aa_4f.root aa_4f/aa_4f-???.root
+
+echo "combine everything"
+hadd alldata.root hmumu.root higgs_ffh.root 2f_all.root 4f_all.root 5f.root aa_4f.root
+
+echo "delete intermediate files"
+rm hmumu.root higgs_ffh.root 2f_all.root 4f_all.root 5f.root aa_4f.root
+
+echo "finished!"
