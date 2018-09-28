21cd5e29c560b6b5e42aea4fbf392821cd41c3df
shkawada <skawada@naf-ilc12.desy.de>
2018-09-28 15:56:03 +0200

    added core files

--- DBD/qqh250/analysis/output/LEFT/BDTG/test/Readme.md
+++ DBD/qqh250/analysis/output/LEFT/BDTG/test/Readme.md
@@ -1,9 +1,12 @@
 # Caution
 This part contains DESY-specific part.
 
-
+# Hou To Use
+0. Assume you have the file named alldata_precuts.root, also this file has 2 trees named datatrain and datatest.
+1. Make following directories: `C/`, `dat/`, `exec/`, `log/`, `root/`, and `scripts/`.
+2. 
 
 # Explanation
-
+This place is used for TMVA analysis, especially to determine the TMVA internal variables.
 
 ## Each file
---
+++ DBD/qqh250/analysis/output/LEFT/BDTG/test/job.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+echo "Job started:" > time.txt
+date >> time.txt
+
+source /afs/desy.de/user/s/skawada/sonas_work/stage_qqh250/analysis/init_ilcsoft.sh
+
+#for nCuts AAAAA
+#for Shrinkage BBBBB
+#for MaxDepth CCCCC
+#for NTrees DDDDD
+#for MinNodeSize EEEEE%
+for cut in {10,20,30}
+do
+    for shrinkage in {0.1,0.15,0.2,0.25,0.3}
+    do
+	for depth in {2,3,4,5}
+	do
+	    for tree in {300,500,800,1000}
+	    do
+		for node in {3,5,10}
+		do
+		    cp HiggsToMuMutrain.C.orig HiggsToMuMutrain.C
+		    sed -i "s/AAAAA/$cut/g" HiggsToMuMutrain.C
+		    sed -i "s/BBBBB/$shrinkage/g" HiggsToMuMutrain.C
+		    sed -i "s/CCCCC/$depth/g" HiggsToMuMutrain.C
+		    sed -i "s/DDDDD/$tree/g" HiggsToMuMutrain.C
+		    sed -i "s/EEEEE/$node/g" HiggsToMuMutrain.C
+		    root -l -b -q HiggsToMuMutrain.C
+		    filename="Result${cut}_${shrinkage}_${depth}_${tree}_${node}.root"
+		    cp alldata_precuts.root $filename
+		    root -l -b << EOF
+.L result.C
+result("$filename")
+EOF
+		done
+	    done
+	done
+    done
+echo "cut ${cut} ended:" >> time.txt
+date >> time.txt
+done
+
+echo "Job ended:" >> time.txt
+date >> time.txt
