c5d9ef154f7526f01ad7b4595fd92c9b7e468771
shkawada <skawada@naf-ilc11.desy.de>
2018-09-29 17:51:46 +0200

    added core files

---
+++ DBD/nnh500/analysis/output/RIGHT/BDTG/count.C
@@ -0,0 +1,62 @@
+void count(){
+  //TFile *file = TFile::Open("alldata_selectvar.root");
+  TFile *file = TFile::Open("alldata_precuts.root");
+  //TFile *file = TFile::Open("alldata_basiccuts.root");
+  int processid;
+  double weight;
+  int type;
+
+  TTree *nttrain = (TTree*)file->Get("datatrain");
+  nttrain->SetBranchAddress( "processid", &processid );
+  nttrain->SetBranchAddress( "weight", &weight );
+  nttrain->SetBranchAddress( "type", &type );
+
+  int Nsigtrain = 0;
+  int Nbkgtrain = 0;
+  double sigtrainweight = 0.;
+  double bkgtrainweight = 0.;
+
+  for( int i = 0; i < nttrain->GetEntries(); i++ ){
+    nttrain->GetEntry(i);
+    if( processid >= 108161 && processid <= 108164 &&
+	( type == 12 || type == 14 || type == 16 ) ){
+      sigtrainweight += weight;
+      Nsigtrain++;
+    }
+    else{
+      bkgtrainweight += weight;
+      Nbkgtrain++;
+    }
+  }
+  cout << "----------For training sample with precuts----------" << endl;
+  cout << "Nsig = " << Nsigtrain << ", Nsig(weight) = " << sigtrainweight << endl;
+  cout << "Nbkg = " << Nbkgtrain << ", Nbkg(weight) = " << bkgtrainweight << endl;
+  cout << endl;
+
+  TTree *nt = (TTree*)file->Get("datatest");
+  nt->SetBranchAddress( "processid", &processid );
+  nt->SetBranchAddress( "weight", &weight );
+  nt->SetBranchAddress( "type", &type );
+
+  int Nsig = 0;
+  int Nbkg = 0;
+  double sigweight = 0.;
+  double bkgweight = 0.;
+
+  for( int i = 0; i < nt->GetEntries(); i++ ){
+    nt->GetEntry(i);
+    if( processid >= 108161 && processid <= 108164 &&
+	( type == 12 || type == 14 || type == 16 ) ){
+      sigweight += weight;
+      Nsig++;
+    }
+    else{
+      bkgweight += weight;
+      Nbkg++;
+    }
+  }
+  cout << "----------For test sample with precuts----------" << endl;
+  cout << "Nsig = " << Nsig << ", Nsig(weight) = " << sigweight << endl;
+  cout << "Nbkg = " << Nbkg << ", Nbkg(weight) = " << bkgweight << endl;
+  cout << endl;
+}
