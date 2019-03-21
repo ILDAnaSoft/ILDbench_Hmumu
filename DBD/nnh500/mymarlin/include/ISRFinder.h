d228798699e6d81fd28f8f8dd71c4a89b164ceab
shkawada <skawada@naf-ilc13.desy.de>
2019-03-21 16:09:54 +0100

    added ISRFinder processor

---
+++ DBD/nnh500/mymarlin/include/ISRFinder.h
@@ -0,0 +1,34 @@
+#ifndef ISRFinder_h
+#define ISRFinder_h 1
+
+#include <string>
+
+#include <marlin/Processor.h>
+#include <lcio.h>
+#include <EVENT/ReconstructedParticle.h>
+
+using namespace lcio;
+using namespace marlin;
+
+class ISRFinder : public Processor{
+ public:
+  virtual Processor* newProcessor() { return new ISRFinder; }
+  ISRFinder();
+  virtual void init();
+  virtual void processRunHeader( LCRunHeader *run );
+  virtual void processEvent( LCEvent* evt );
+  virtual void check( LCEvent* evt );
+  virtual void end();
+
+ protected:
+  std::string _inputCollection;
+  std::string _allPFOCollection;
+  std::string _outputCollection;
+  std::string _isrCollection;
+
+  float _cosTheta;
+  float _energy;
+  float _coneCosTheta;
+  float _ratio;
+};
+#endif
