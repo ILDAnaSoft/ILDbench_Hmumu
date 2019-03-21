e3ee286658df3c11af9392d3be9534bdce44d0db
shkawada <skawada@naf-ilc13.desy.de>
2019-03-21 13:54:30 +0100

    simple check added: check the evets are properly processed

---
+++ DBD/nnh250/analysis/check.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+
+for process in {hmumu,higgs_ffh,2f_Z_bhabhag,2f_Z_hadronic,2f_Z_leptonic,4f_singleW_leptonic,4f_singleW_semileptonic,4f_singleZee_leptonic,4f_singleZee_semileptonic,4f_singleZnunu_leptonic,4f_singleZnunu_semileptonic,4f_singleZsingleWMix_leptonic,4f_WW_hadronic,4f_WW_leptonic,4f_WW_semileptonic,4f_ZZ_hadronic,4f_ZZ_leptonic,4f_ZZ_semileptonic,4f_ZZWWMix_hadronic,4f_ZZWWMix_leptonic,aa_2f,1f_3f}
+do
+    echo "check for ${process}"
+    ls ./tmp/${process}/*output.log | wc -l
+    grep "END OF PROCESSING" ./tmp/${process}/*output.log | wc -l
+done
