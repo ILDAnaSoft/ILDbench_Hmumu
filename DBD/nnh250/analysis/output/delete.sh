1302c16ce2670ff1d455a5a64ba4068a5395d53e
shkawada <skawada@naf-ilc11.desy.de>
2020-02-13 14:24:52 +0100

    modified

--- DBD/nnh250/analysis/output/delete.sh
+++ DBD/nnh250/analysis/output/delete.sh
@@ -2,10 +2,4 @@
 
 cd /afs/desy.de/user/s/skawada/sonas_work/stage_nnh250/analysis/output
 
-rm *~
-var=0
-if [ $var -eq 1 ]; then
-rm *.root *~ LEFT/*.root RIGHT/*.root
-rm LEFT/*.log LEFT/BDTG/*.root LEFT/BDTG/test/*.root LEFT/BDTG/test/Analysis_result.dat LEFT/BDTG/test/*/* LEFT/BDTG/125fix5/*.root
-rm RIGHT/*.log RIGHT/BDTG/*.root RIGHT/BDTG/test/*.root RIGHT/BDTG/test/Analysis_result.dat RIGHT/BDTG/test/*/* RIGHT/BDTG/125fix5/*.root
-fi
+rm *.root LEFT/*.root RIGHT/*.root
